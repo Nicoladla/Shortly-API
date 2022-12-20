b7d2f97291cd594dc2416ea641f35e9aa4dc8b2e
Nicolas Araújo <nicolasaraujo@gmail.com>
2022-12-20 20:18:08 -0300

    feat: configurações iniciais do index e da conexão com o banco.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT;
+app.listen(port, () => console.log(`App running on port ${port}`));
