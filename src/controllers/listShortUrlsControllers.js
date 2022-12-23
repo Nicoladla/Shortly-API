7c5c26333de24307960fd591ee82dc177560e2a6
Nicolas Araújo <nicolasaraujo@gmail.com>
2022-12-23 15:45:51 -0300

    feat: começado da rota de rankingShortUrls

--- src/controllers/listShortUrlsControllers.js
+++ src/controllers/listShortUrlsControllers.js
@@ -1,12 +1,23 @@
 import connection from "../database/db.js";
 
 export function MyshortUrlsGet(req, res) {
-    const {id: userId}= res.locals.user
+  const { id: userId } = res.locals.user;
 
   try {
-    const myShortUrls = connection.query(`SELECT * FROM`)
+    const myShortUrls = connection.query(`SELECT * FROM`);
 
-    res.status(200).send(myShortUrls)
+    res.status(200).send(myShortUrls);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+}
+
+export function rankingGet(req, res) {
+  try {
+    const shortUrls = connection.query(`SELECT * FROM`);
+
+    res.status(200).send(shortUrls);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
--- src/routers/listShortUrlsRouters.js
+++ src/routers/listShortUrlsRouters.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import tokenValidation from "../middleware/tokenValidationMiddleware.js";
-import { MyshortUrlsGet } from "../controllers/listShortUrlsControllers.js";
+import { MyshortUrlsGet, rankingGet } from "../controllers/listShortUrlsControllers.js";
 
 const router = Router();
 
 router.get("/users/me", tokenValidation, MyshortUrlsGet);
+router.get("/ranking", rankingGet);
 
 export default router;
